d7d84c6a76af3e16a421d4a6caf46c9422657eaa
Liz Lam <lizlam.217@gmail.com>
2020-09-18 13:12:54 -0700

    initial add

---
+++ src/Game.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  div.map {
+    image-rendering: pixelated;
+    background-image: url("/images/test.png");
+    background-repeat: no-repeat;
+    background-size: 100% auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  Chapter 1
+  <div class="map" />
+</main>
